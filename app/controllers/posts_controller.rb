42b363304539b613b433b609fcbe393c43c78265
koyama1058 <koyama1058tk@gmail.com>
2020-11-05 19:41:03 +0900

    新規投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      @room = Room.create(post_id: @post.id)
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :title, :category_id, :description, :day_time, :prefectures_id, :place, :budget).merge(user_id: current_user.id)
   end
 end
