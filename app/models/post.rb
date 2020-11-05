013f8c7a34dbf3cc709539650f910c5a58c68019
koyama1058 <koyama1058tk@gmail.com>
2020-11-05 19:54:13 +0900

    userpostのアソシエーションとバリデーションを記入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :prefectures
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :category_id, numericality: { other_than: 1 }
+    validates :description
+    validates :day_time
+    validates :prefectures_id
+    validates :place
+    validates :budget
+  end
 end
