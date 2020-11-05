df7dbea4cd94d8b98e07f0dc5a9f419975ef5187
koyama1058 <koyama1058tk@gmail.com>
2020-11-05 18:49:34 +0900

    userのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         extend ActiveHash::Associations::ActiveRecordExtensions
+         belongs_to_active_hash :gender
+         belongs_to_active_hash :prefectures
+         belongs_to_active_hash :job
+
+         with_options presence: true do
+          validates :nickname
+        end
 end
