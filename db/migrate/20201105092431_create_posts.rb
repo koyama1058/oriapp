26bfcd958dcd0c1b24b5b63a3edd67bd6f2b44cd
koyama1058 <koyama1058tk@gmail.com>
2020-11-05 18:32:06 +0900

    トップページの作成

--- db/migrate/20201105092431_create_posts.rb
+++ db/migrate/20201105092431_create_posts.rb
@@ -1,7 +1,14 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :title,          null: false
+      t.integer :category_id,   null: false
+      t.text :description,      null: false
+      t.string :day_time
+      t.integer :prefectures_id
+      t.text :place
+      t.text :budget
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_092235) do
+ActiveRecord::Schema.define(version: 2020_11_05_092431) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "category_id", null: false
+    t.text "description", null: false
+    t.string "day_time"
+    t.integer "prefectures_id"
+    t.text "place"
+    t.text "budget"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -31,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_11_05_092235) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
