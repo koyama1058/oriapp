469a7af929a216d7dffa468b4263e4af84743ea8
koyama1058 <koyama1058tk@gmail.com>
2020-11-05 19:09:06 +0900

    ユーザー編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.new(user_params)
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:image, :nickname, :email, :introduction, :job_id, :hobby, :prefectures_id, :gender_id, :birthday)
+  end
 end
