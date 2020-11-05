9eeeaedeefa497228740b49fea68254d016c87fa
koyama1058 <koyama1058tk@gmail.com>
2020-11-05 19:06:16 +0900

    ユーザー詳細画面の導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         extend ActiveHash::Associations::ActiveRecordExtensions
-         belongs_to_active_hash :gender
-         belongs_to_active_hash :prefectures
-         belongs_to_active_hash :job
+  has_one_attached :image
 
-         with_options presence: true do
-          validates :nickname
-        end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :gender
+  belongs_to_active_hash :prefectures
+  belongs_to_active_hash :job
+
+  with_options presence: true do
+  validates :nickname
+  end
 end
