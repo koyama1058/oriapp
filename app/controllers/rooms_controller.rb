227edcf72ef305d547292932f2c7488df1967049
koyama1058 <koyama1058tk@gmail.com>
2020-11-06 12:37:29 +0900

    チャットタイトルの表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,14 @@
 class RoomsController < ApplicationController
 
   def index
+    # post_idはroom_idのこと
     room_user = RoomUser.new(room_id: params[:post_id],user_id: current_user.id)
     if RoomUser.where(room_id: params[:post_id],user_id: current_user.id) == []
       room_user.save
     end
+    @message = Message.new
+    # post_idはroom_idのこと
+    @room = Room.find(params[:post_id])
   end
 
 
