83717d229d5b3548e3563a720b88820f44ff2aa5
koyama1058 <koyama1058tk@gmail.com>
2020-11-07 14:02:18 +0900

    good_usersテーブルの作成

--- db/migrate/20201107050048_create_good_users.rb
+++ db/migrate/20201107050048_create_good_users.rb
@@ -1,7 +1,7 @@
 class CreateGoodUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :good_users do |t|
-
+      t.references :user,   null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_025606) do
+ActiveRecord::Schema.define(version: 2020_11_07_050048) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2020_11_07_025606) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_favorites_on_post_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "good_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_good_users_on_user_id"
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -97,6 +104,7 @@ ActiveRecord::Schema.define(version: 2020_11_07_025606) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "favorites", "posts"
   add_foreign_key "favorites", "users"
+  add_foreign_key "good_users", "users"
   add_foreign_key "messages", "posts"
   add_foreign_key "messages", "users"
   add_foreign_key "post_users", "posts"
