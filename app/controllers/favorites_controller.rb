3e2ee4c1919ebd5b377b61d5498a60ebb65a22f5
koyama1058 <koyama1058tk@gmail.com>
2020-11-07 13:29:22 +0900

    いいねの保存・削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,18 @@
 class FavoritesController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @favorite = current_user.favorites.create(post_id: params[:post_id])
+    redirect_to  post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @favorite = current_user.favorites.find_by(post_id: @post.id)
+    if @favorite.destroy
+      redirect_to post_path(@post)
+    else 
+      redirect_to post_path(@post)
+    end
+  end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,5 +3,4 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :post_id,  uniquness: { scope: [:post_id, :user_id]  }
 end
