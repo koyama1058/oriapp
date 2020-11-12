e0a50fb5ca28b000f371ca3b2568ee5611e8117e
koyama1058 <koyama1058tk@gmail.com>
2020-11-12 12:15:14 +0900

    投稿が削除された時にpost_tagsも一緒に削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   has_many :post_users
   has_many :users, through: :post_users
   has_many :favorites, dependent: :destroy
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   extend ActiveHash::Associations::ActiveRecordExtensions
