efb9c4ae9d6c907326883e7f63863a0593b4ba8a
koyama1058 <koyama1058tk@gmail.com>
2020-11-16 12:43:43 +0900

    addressをファイヤーウォールに追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -89,7 +89,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:posts_tag).permit(:image, :title, :category_id, :description, :day_time, :prefectures_id, :place, :budget, :name).merge(user_id: current_user.id)
+    params.require(:posts_tag).permit(:address, :image, :title, :category_id, :description, :day_time, :prefectures_id, :place, :budget, :name).merge(user_id: current_user.id)
   end
 
   def update_params
