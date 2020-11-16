dfbc4be9e2c6fab27398b405a13dc94746395330
koyama1058 <koyama1058tk@gmail.com>
2020-11-16 11:44:01 +0900

    マイグレーションファイルの作成

--- db/migrate/20201116024137_create_spots.rb
+++ db/migrate/20201116024137_create_spots.rb
@@ -1,7 +1,10 @@
 class CreateSpots < ActiveRecord::Migration[6.0]
   def change
     create_table :spots do |t|
-
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.references :post, foreign_key: true, null: false
       t.timestamps
     end
   end
