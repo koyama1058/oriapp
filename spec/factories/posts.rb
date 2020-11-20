6c5cc097fdc49e444abe8db05fd21984fc9c9ad5
koyama1058 <koyama1058tk@gmail.com>
2020-11-20 12:18:39 +0900

    factoriesの生成

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :post do
-    
+    image           {"image.png"}
+    title           {"テスト"}
+    category_id     {3}
+    description     {"詳細テスト"}
+    day_time        {"1994.08.02"}
+    prefectures_id  {3}
+    place           {"場所テスト"}
+    budget          {"予算テスト"}
+    association               :user
+    after(:build) do |culture|
+      culture.image.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'sample.jpg')), filename: 'sample.jpg', content_type: 'image/jpg')
+    end
   end
 end
